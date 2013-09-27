d21eaae0383074a03ea836b91eab6b014f89d764
Juan Posadas <jposadas@stanford.edu>
2013-09-27 01:11:51 -0700

    Initial Commit

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Juan Posadas on 9/27/13.
+//  Copyright (c) 2013 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
